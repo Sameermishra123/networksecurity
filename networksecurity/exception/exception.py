a06ecc0439f1c5f84bc315e221b521b938dde6d3
SAMEER MISHRA <sameermishra280202@gmail.com>
2025-02-13 14:25:34 +0530

    Exception handing and logginh handling

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -1,9 +1,20 @@
 import sys
+from networksecurity.logging import logger
 class  NetworkSecurityException(Exception):
-    def __init__(self, message):
-        self.message = message
-        super().__init__(self.message)
+    def __init__(self,error_message,error_details:sys):
+        self.error_message=error_message
+        _,_,exc_tb=error_details.exc_info()
+
+        self.lineno=exc_tb.tb_lineno
+        self.file_name=exc_tb.tb_frame.f_code.co_filename
+
     def __str__(self):
-        return self.message
-    def __repr__(self):
-        return self.message
+        return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(self.file_name,self.lineno,self.error_message)
+    
+if __name__=="__main__":
+    try:
+        logger.logging.info("This is a test message")
+        a=1/0
+        print("THIS WILL NOT BE PRINTED",a)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
